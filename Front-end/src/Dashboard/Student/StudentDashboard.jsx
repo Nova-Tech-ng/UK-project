3c81fec44a9c049642575aee505ac905f967a598
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2024-07-31 09:33:52 +0100

    made adjustments

--- Front-end/src/Dashboard/Student/StudentDashboard.jsx
+++ Front-end/src/Dashboard/Student/StudentDashboard.jsx
@@ -1,5 +1,6 @@
+import React, { useState } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import StudentLearningResource from "./StudentLearningResource";
 import {
   BarChart,
   Bar,
@@ -17,64 +18,20 @@ const data = [
   { year: "2023", PredictedGrade: 60, ActualGrade: 55 },
   { year: "2024", PredictedGrade: 70, ActualGrade: 40 },
 ];
-export const studentData = [
-  {
-    id: 194,
-    gender: "Female",
-    name: "Grace Evans",
-    PredictedGrade: 89,
-    attendance: 89,
-    assignment: 89,
-    gpa: 3.74,
-    status: "Pending",
-  },
-  {
-    id: 233,
-    gender: "Male",
-    name: "Maro Oghenerukwe",
-    PredictedGrade: 90,
-    attendance: 94,
-    assignment: 94,
-    gpa: 3.92,
-    status: "Pending",
-  },
-  {
-    id: 921,
-    gender: "Male",
-    name: "Matthew Jonathan",
-    PredictedGrade: 75,
-    attendance: 25,
-    assignment: 25,
-    gpa: 1.45,
-    status: "At Risk",
-  },
-  {
-    id: 183,
-    gender: "Female",
-    name: "Glory Evans",
-    PredictedGrade: 76,
-    attendance: 56,
-    assignment: 56,
-    gpa: 2.85,
-    status: "Pending",
-  },
-  {
-    id: 219,
-    gender: "Male",
-    name: "Wila Amirs",
-    PredictedGrade: 55,
-    attendance: 62,
-    assignment: 62,
-    gpa: 3.02,
-    status: "pending",
-  },
-];
 
 const StudentDashboard = () => {
+  const [activeTab, setActiveTab] = useState("dashboard");
+
   const routes = [
-    { path: "/dashboard", name: "Individual Performance" },
-    { path: "/learning-resources", name: "Learning Resources" },
+    { id: "dashboard", name: "Individual Performance" },
+    { id: "learning-resources", name: "Learning Resources" },
   ];
+
+  const handleNavigation = (id) => {
+    setActiveTab(id);
+    window.history.pushState({}, "", `/${id}`);
+  };
+
   return (
     <div className="container p-4 mx-auto">
       {/* ADMIN ICON */}
@@ -85,72 +42,81 @@ const StudentDashboard = () => {
       {/* NAVIGATION */}
       <div className="space-x-4">
         {routes.map((route) => (
-          <NavLink
-            key={route.path}
-            to={route.path}
-            className={({ isActive }) =>
-              isActive ? "text-[#D25D09] font-semibold" : ""
+          <button
+            key={route.id}
+            onClick={() => handleNavigation(route.id)}
+            className={
+              activeTab === route.id
+                ? "text-[#D25D09] font-semibold transition-colors"
+                : ""
             }
           >
             {route.name}
-          </NavLink>
+          </button>
         ))}
       </div>
-      {/* DASHBOARD */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mb-4 mt-4">
-        <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
-          <span className="ml-2 font-semibold">Predicted Grade</span>
-          <div className="flex justify-end">
-            <p className="text-5xl font-semibold">54%</p>
-          </div>
-        </div>
-        <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
-          <span className="ml-2 font-semibold">Actual Grade</span>
-          <div className="flex justify-end">
-            <p className="text-5xl font-semibold">72.43%</p>
-          </div>
-        </div>
-        <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
-          <span className="ml-2 font-semibold">Attendance Rate</span>
-          <div className="flex justify-end">
-            <p className="text-5xl font-semibold">80%</p>
-          </div>
-        </div>
-        <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
-          <span className="ml-2 font-semibold">Risk Indicator</span>
-          <div className="flex justify-end">
-            <div className="w-full bg-gray-200  relative">
-              <div
-                className="h-8 "
-                style={{
-                  width: "100%",
-                  background: "linear-gradient(to right, green, red)",
-                }}
-              ></div>
-              <div className="absolute top-0 left-3/4 transform -translate-x-1/2 text-red-500 text-sm">
-                At risk
+      {/* CONTENT */}
+      {activeTab === "dashboard" && (
+        <>
+          {/* DASHBOARD */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mb-4 mt-4">
+            <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
+              <span className="ml-2 font-semibold">Predicted Grade</span>
+              <div className="flex justify-end">
+                <p className="text-5xl font-semibold">54%</p>
+              </div>
+            </div>
+            <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
+              <span className="ml-2 font-semibold">Actual Grade</span>
+              <div className="flex justify-end">
+                <p className="text-5xl font-semibold">72.43%</p>
+              </div>
+            </div>
+            <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
+              <span className="ml-2 font-semibold">Attendance Rate</span>
+              <div className="flex justify-end">
+                <p className="text-5xl font-semibold">80%</p>
+              </div>
+            </div>
+            <div className="border p-2 rounded shadow-lg flex flex-col justify-around">
+              <span className="ml-2 font-semibold">Risk Indicator</span>
+              <div className="flex justify-end">
+                <div className="w-full bg-gray-200 relative">
+                  <div
+                    className="h-8"
+                    style={{
+                      width: "100%",
+                      background:
+                        "linear-gradient(to right, green 80%, red 20%)",
+                    }}
+                  ></div>
+                  <div className="absolute top-0 left-3/4 transform -translate-x-1/2 text-red-500 text-sm">
+                    At risk
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      {/* CHART */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 ">
-        <div className="border p-4 rounded shadow-lg">
-          <h2 className="text-center mb-4">Performance Over Time</h2>
-          <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="year" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="PredictedGrade" fill="#D25D09" />
-              <Bar dataKey="ActualGrade" fill="#4567B7" />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-      </div>
+          {/* CHART */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+            <div className="border p-4 rounded shadow-lg">
+              <h2 className="text-center mb-4">Performance Over Time</h2>
+              <ResponsiveContainer width="100%" height={300}>
+                <BarChart data={data}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="year" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="PredictedGrade" fill="#D25D09" />
+                  <Bar dataKey="ActualGrade" fill="#4567B7" />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        </>
+      )}
+      {activeTab === "learning-resources" && <StudentLearningResource />}
     </div>
   );
 };
